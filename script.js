2738e01be20953f3f9ca94af952d8cf2288e326d
Charles Roper <charles@roper.im>
2023-04-30 23:58:28 +0100

    Make script load async again

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var script = document.createElement("script");
 script.src =
   "https://maps.googleapis.com/maps/api/js?key=AIzaSyAOeOAjAuHro9ZiUFRD9FpHZPqtbxRc2xc&callback=initMap";
-script.async = false;
+script.async = true;
 
 // Attach your callback function to the `window` object
 window.initMap = function () {
