68bff1d7fcece39c7f4ed40ca81995136bfd8a42
Charles Roper <charles@roper.im>
2023-05-12 05:24:04 +0100

    Change API key
    Changes to key in fscwebmaster1943 > FSC Main Website

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Create the script tag, set the appropriate attributes
 const script = document.createElement("script");
 script.src =
-  "https://maps.googleapis.com/maps/api/js?key=AIzaSyAOeOAjAuHro9ZiUFRD9FpHZPqtbxRc2xc&callback=initMap";
+  "https://maps.googleapis.com/maps/api/js?key=AIzaSyBHS4GGgHG3P1HWz0qMcl24l2MlL-gSHXQ&callback=initMap";
 script.async = true;
 
 // Attach your callback function to the `window` object
